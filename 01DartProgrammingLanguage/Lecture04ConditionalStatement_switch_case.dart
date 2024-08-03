f6ca50acf499db780f08c06ffcc62c39a5837e45
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2024-08-03 22:57:51 +0500

    code updated

---
+++ 01DartProgrammingLanguage/Lecture04ConditionalStatement_switch_case.dart
@@ -0,0 +1,42 @@
+void main() {
+  String vowels = "H";
+
+  switch (vowels) {
+    case 'A':
+    case 'a':
+      {
+        print("its a vowel");
+        break;
+      }
+    case 'E':
+    case 'e':
+      {
+        print("its a vowel");
+        break;
+      }
+    case 'I':
+    case 'i':
+      {
+        print("its a vowel");
+        break;
+      }
+
+    case 'O':
+    case 'o':
+      {
+        print("its a vowel");
+        break;
+      }
+    case 'U':
+    case 'u':
+      {
+        print("its a vowel");
+        break;
+      }
+    default:
+    {
+      print("its not a vowel");
+      break;
+    }
+  }
+}
