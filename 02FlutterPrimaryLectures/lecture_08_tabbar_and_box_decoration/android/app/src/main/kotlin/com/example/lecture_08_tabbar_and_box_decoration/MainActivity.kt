fdd76243a38f23f0814d1ec0c11a3d85d0d92137
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2024-09-10 20:50:34 +0500

    code updated

---
+++ 02FlutterPrimaryLectures/lecture_08_tabbar_and_box_decoration/android/app/src/main/kotlin/com/example/lecture_08_tabbar_and_box_decoration/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lecture_08_tabbar_and_box_decoration
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ 02FlutterPrimaryLectures/lecture_08_tabbar_and_box_decoration/lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text("GLS2 | Chat App"),
+          ),
+          bottom: TabBar(
+            tabs: [
+              Tab(child: Text("Status")),
+              Tab(child: Text("Chats")),
+              Tab(child: Text("Reports"))
+            ],
+            labelColor: Colors.teal,
+            unselectedLabelColor: Colors.greenAccent,
+          ),
+        ),
+        body: TabBarView(children: [Status(), Chats(), Reports()]),
+      ),
+    );
+  }
+}
+
+class Reports extends StatefulWidget {
+  const Reports({super.key});
+
+  @override
+  State<Reports> createState() => _ReportsState();
+}
+
+class _ReportsState extends State<Reports> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Reports")));
+  }
+}
+
+class Status extends StatefulWidget {
+  const Status({super.key});
+
+  @override
+  State<Status> createState() => _StatusState();
+}
+
+class _StatusState extends State<Status> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Status")));
+  }
+}
+
+class Chats extends StatefulWidget {
+  const Chats({super.key});
+
+  @override
+  State<Chats> createState() => _ChatsState();
+}
+
+class _ChatsState extends State<Chats> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Chats")));
+  }
+}
