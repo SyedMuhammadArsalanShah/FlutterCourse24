b7b124ba11f6c3d2d32a1d044c0fa9e3e5b4e75d
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2024-09-22 18:27:44 +0500

    lecture 11

--- 02FlutterPrimaryLectures/lecture11expanded_wrap_stack_positioned_align/android/app/src/main/kotlin/com/example/lecture11expanded_wrap_stack_positioned_align/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.lecture11expanded_wrap_stack_positioned_align
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ 02FlutterPrimaryLectures/lecture_11_expanded_wrap_stack_positioned_align/android/app/src/main/kotlin/com/example/lecture_11_expanded_wrap_stack_positioned_align/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lecture_11_expanded_wrap_stack_positioned_align
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
